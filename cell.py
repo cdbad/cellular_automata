fa516e201f3efedfb479ed291941b0890bf317a8
natdeleon <nat.deleonf@gmail.com>
2024-06-20 14:03:24 +0200

    first test

--- cell.py
+++ cell.py
@@ -1,7 +1,10 @@
 class CellClass:
-    def __init__(self, rule) -> None:
-        self.rule = str(bin(int(rule)))
-
+    def __init__(self, rule: str) -> None:
+        self.rule = str(bin(int(rule)))[2:]
+      
         if len(self.rule) < 8:
             s = 8 - len(self.rule)
             self.rule = ('0'*s) + self.rule
+
+
+x = CellClass('22')
