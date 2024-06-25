de80fa7146636a5e6755db3ffbf145f681ed5e8d
natdeleon <nat.deleonf@gmail.com>
2024-06-25 11:29:54 +0200

    integration and clean

--- cell_call.py
+++
@@ -1,6 +0,0 @@
-from cell import CellClass
-
-celular_automata = CellClass(123, (40,40))
-
-print(f'binary rule: {celular_automata.rule} \nsize: {celular_automata.x} x {celular_automata.y}')
-matrix = celular_automata.generate_cells()
