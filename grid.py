df29f84e2af73c3528b2b190284cc6faa274dbcb
Cody <cdbadiani@gmail.com>
2024-06-25 14:47:23 +0200

    Natalias code

--- grid.py
+++ grid.py
@@ -3,11 +3,19 @@ from cell import CellClass
 
 
 class Grid:
-
     def __init__(self, rule, rows, cols) -> None:
+        # try:
         # Windown Display
+        self.rule = rule
+        self.rows = rows
+        self.cols = cols
+
+        cellular_automata = CellClass(self.rule, (self.rows, self.cols))
+        self.matrix = cellular_automata.generate_cells()
+
+    def make_screen(self):
         self.wn = turtle.Screen()
-        self.wn.bgcolor("light green")
+        self.wn.bgcolor("#F2E86D")
         self.wn.title("Cellular Automata")
         self.wn.setup(1000, 1000)  # window size
 
@@ -16,11 +24,6 @@ class Grid:
         self.drawer.speed(400)
         self.drawer.hideturtle()
         
-        self.rule = rule
-        self.rows = rows
-        self.cols = cols
-        celular_automata = CellClass(self.rule, (self.rows, self.cols))
-        self.matrix = celular_automata.generate_cells()
         self.create_grid()
 
         #self.wn.tracer(40)
@@ -28,8 +31,11 @@ class Grid:
 
         # Hide the turtle and keep the window open until it is clicked
         self.drawer.hideturtle()
+        
         self.wn.exitonclick()
-
+        # turtle.TurtleScreen._RUNNING = True
+        # except turtle.Terminator:
+        #     pass
 
 # Function to draw a filled square at a specific position
 # It will allow us to draw in the middel of the panel
@@ -56,8 +62,8 @@ class Grid:
                 x = start_x + col * square_size
                 y = start_y - row * square_size
                 if row_in_matrix[col] == 1:
-                    row_in_matrix[col] = 'purple'
+                    row_in_matrix[col] = '#422040'
                 else:
-                    row_in_matrix[col] = 'white'
+                    row_in_matrix[col] = '#E9F1F7'
                 color = row_in_matrix[col]
                 self.draw_filled_square(x, y, square_size * 0.8, color)
--- menu.py
+++ menu.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 from grid import Grid
+import turtle
 
 class Home:
-    
     def __init__(self):
         # make window
         self.root = tk.Tk()
@@ -76,6 +76,8 @@ class Home:
         width = self.w_entry.get()
         height = self.h_entry.get()
 
+        
+
         if not rule or not width or not height:
             self.__error_label('All the fields must be filled')
         elif type(rule) == str and type(width) == str and type(height) == str:
@@ -83,6 +85,13 @@ class Home:
                 rule = int(rule)
                 width = int(width)
                 height = int(height)
-                Grid(rule, width, height)
+                print(rule, width, height)
+                self.visual = Grid(rule, width, height)
+                self.visual.make_screen()
             except ValueError:
                 self.__error_label('All entries must be numbers')
+            except turtle.Terminator:
+                del self.visual
+
+    # def delete_window(self):
+    #     del self.visual
